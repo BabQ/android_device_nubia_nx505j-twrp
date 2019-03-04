792e987f1fdcf3956ac390b2c46cb9f687b6636d
BabQ <1153791778@qq.com>
2019-03-04 22:09:22 +0800

    NX505J:Add information about nx505j in README.mk

---
+++ README.mk
@@ -0,0 +1,23 @@
+Team Win Recovery Project
+
+Device configuration for Nubia z7 Max  (_nx505j_)
+=====================================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     |  Quad-core 2.5 GHz Krait 400
+CHIPSET | Qualcomm MSM8974AC
+GPU     | Adreno 330
+Memory  | 2 GB
+Shipped Android Version | Android 4.4
+Storage | 32 GB
+MicroSD | Up to 64 GB (Hybrid)
+Battery | 3100 mAh (non-removable)
+Dimensions | 152.7 x 76 x 8.7 mm
+Display | 1920 x 1080 pixels, 5.5" IPS
+Rear Camera  | 13.0 MP, LED flash
+Front Camera | 5.0 MP
+Release Date | July 2014
+
+![Nubia Z7MAX](http://cdn2.gsmarena.com/vv/pics/zte/zte-nubia-z7-max-2.jpg "Nubia Z7MAX")
+
